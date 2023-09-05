558a1ec45c19851eb315e6b815dedd0a60359f7e
Matteo Collina <hello@matteocollina.com>
2023-09-05 19:23:21 +0200

    working
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 import fp from 'fastify-plugin'
 import tags from 'common-tags'
 
+const kLayout = Symbol('fastifyHtmlLayout')
+
+class Layout {
+  constructor (render, parent) {
+    this._render = render
+    if (parent) {
+      this.parent = parent
+    }
+  }
+
+  render (inner) {
+    const fn = this._render
+    let res = fn(inner)
+    if (this.parent) {
+      res = this.parent.render(res)
+    }
+    return res
+  }
+}
+
 export default fp(async (fastify, opts) => {
-  let layout = null
   fastify.decorate('tags', tags)
+  fastify.decorate(kLayout, null)
 
   fastify.decorate('addLayout', function (render) {
-    layout = render
+    // Using a symbol attached to `this` and a stack allows us to
+    // support nested layouts with encapsulated plugins.
+    let layout = this[kLayout]
+    layout = new Layout(render, layout)
+    this[kLayout] = layout
   })
 
   fastify.decorateReply('html', function (strings, ...values) {
     let html = tags.html.call(null, strings, ...values)
+    const layout = this.server[kLayout]
 
     if (layout) {
-      html = layout(html)
+      html = layout.render(html)
     }
 
     this.header('Content-Type', 'text/html; charset=utf-8')
--- test.js
+++ test.js
@@ -94,3 +94,137 @@ test('one level layout', async t => {
 </html>`)
   }
 })
+
+test('two levels layout', async t => {
+  const app = fastify()
+  await app.register(fastifyHtml)
+
+  app.addLayout(function (inner) {
+    return app.tags.html`
+      <!DOCTYPE html>
+      <html lang="en">
+        <body>
+          ${inner}
+        </body>
+      </html>
+    `
+  })
+
+  app.addLayout(function (inner) {
+    return app.tags.html`
+      <div>
+        ${inner}
+      </div>
+    `
+  })
+
+  app.get('/', async (req, reply) => {
+    const name = req.query.name || 'World'
+    strictEqual(reply.html`<h1>Hello ${name}</h1>`, reply)
+    return reply
+  })
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, `<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  </body>
+</html>`)
+  }
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/?name=Matteo'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, `<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div>
+      <h1>Hello Matteo</h1>
+    </div>
+  </body>
+</html>`)
+  }
+})
+
+test('two levels layout with plugins', async t => {
+  const app = fastify()
+  await app.register(fastifyHtml)
+
+  app.addLayout(function (inner) {
+    return app.tags.html`
+      <!DOCTYPE html>
+      <html lang="en">
+        <body>
+          ${inner}
+        </body>
+      </html>
+    `
+  })
+
+  app.get('/', async (req, reply) => {
+    const name = req.query.name || 'World'
+    strictEqual(reply.html`<h1>Hello ${name}</h1>`, reply)
+    return reply
+  })
+
+  app.register(async function (app) {
+    app.addLayout(function (inner) {
+      return app.tags.html`
+        <div>
+          ${inner}
+        </div>
+      `
+    })
+
+    app.get('/inner', async (req, reply) => {
+      const name = req.query.name || 'World'
+      strictEqual(reply.html`<h1>Hello ${name}</h1>`, reply)
+      return reply
+    })
+  })
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, `<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <h1>Hello World</h1>
+  </body>
+</html>`)
+  }
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/inner?name=Matteo'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, `<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <div>
+      <h1>Hello Matteo</h1>
+    </div>
+  </body>
+</html>`)
+  }
+})
