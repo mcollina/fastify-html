6b0bb9781f463390665a548dd8c945c0ff730ae8
Matteo Collina <hello@matteocollina.com>
2023-09-05 19:07:16 +0200

    layout
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ import fp from 'fastify-plugin'
 import tags from 'common-tags'
 
 export default fp(async (fastify, opts) => {
+  let layout = null
   fastify.decorate('tags', tags)
 
+  fastify.decorate('addLayout', function (render) {
+    layout = render
+  })
+
   fastify.decorateReply('html', function (strings, ...values) {
-    const html = tags.html.call(null, strings, ...values)
+    let html = tags.html.call(null, strings, ...values)
+
+    if (layout) {
+      html = layout(html)
+    }
 
     this.header('Content-Type', 'text/html; charset=utf-8')
     this.send(html)
--- test.js
+++ test.js
@@ -42,3 +42,55 @@ test('expose tags', async t => {
 
   strictEqual(app.tags, tags)
 })
+
+test('one level layout', async t => {
+  const app = fastify()
+  await app.register(fastifyHtml)
+
+  app.addLayout(function (inner) {
+    return app.tags.html`
+      <!DOCTYPE html>
+      <html lang="en">
+        <body>
+          ${inner}
+        </body>
+      </html>
+    `
+  })
+
+  app.get('/', async (req, reply) => {
+    const name = req.query.name || 'World'
+    strictEqual(reply.html`<h1>Hello ${name}</h1>`, reply)
+    return reply
+  })
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, `<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <h1>Hello World</h1>
+  </body>
+</html>`)
+  }
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/?name=Matteo'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, `<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <h1>Hello Matteo</h1>
+  </body>
+</html>`)
+  }
+})
