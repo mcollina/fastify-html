56f74df8ede1dbe820c68d99e49e300ffcd50918
Matteo Collina <hello@matteocollina.com>
2023-09-05 23:49:59 +0200

    added htmx example
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ examples/app.js
@@ -0,0 +1,39 @@
+import fastify from 'fastify'
+import fastifyHtml from '../index.js'
+import formBody from '@fastify/formbody'
+
+const app = fastify({ logger: true })
+await app.register(formBody)
+await app.register(fastifyHtml)
+
+app.addLayout(function (inner, reply) {
+  return app.tags.html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <script src="https://unpkg.com/htmx.org@1.9.5"></script>
+      </head>
+      <body>
+        ${inner}
+      </body>
+    </html>
+  `
+})
+
+app.get('/', async (req, reply) => {
+  const name = req.query.name || 'World'
+  return reply.html`
+    <h1>Hello ${name}</h1>
+    <button hx-post="/clicked" hx-swap="outerHTML">
+      Click Me
+    </button>
+  `
+})
+
+app.post('/clicked', async (req, reply) => {
+  return reply.html`
+    <h1>Clicked</h1>
+  `
+})
+
+await app.listen({ port: 3000 })
