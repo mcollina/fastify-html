a011242b30151c769bf1f244c77860be278d6155
Matteo Collina <hello@matteocollina.com>
2024-01-18 14:56:36 +0100

    Support Node 18
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ import { test } from 'node:test'
 import fastify from 'fastify'
 import fastifyHtml from './index.js'
 import { strictEqual } from 'node:assert'
+import { escapeDictionary } from './lib/html.js'
 
 test('render html', async t => {
   const app = fastify()
@@ -255,6 +256,7 @@ test('use reply in the layout', async t => {
   </body>
 </html>`.replaceAll(' ', '').replaceAll('\n', ''))
 })
+
 test('skip layout with hx-request', async t => {
   const app = fastify()
   await app.register(fastifyHtml)
@@ -286,4 +288,117 @@ test('skip layout with hx-request', async t => {
   strictEqual(res.statusCode, 200)
   strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
   strictEqual(res.body, '<h1>Hello World</h1>')
+})
+
+test('render arrays', async t => {
+  const app = fastify()
+  app.register(fastifyHtml)
+
+  app.get('/', async (req, reply) => {
+    const name = req.query.name || 'World'
+    strictEqual(reply.html`<h1>${['Hello', ' ', name]}</h1>`, reply)
+    return reply
+  })
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, '<h1>Hello World</h1>')
+  }
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/?name=Matteo'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, '<h1>Hello Matteo</h1>')
+  }
+})
+
+test('renders an empty tag', async t => {
+  const app = fastify()
+  app.register(fastifyHtml)
+
+  app.get('/', async (_req, reply) => {
+    strictEqual(reply.html``, reply)
+    return reply
+  })
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, '')
+  }
+})
+
+test('renders null', async t => {
+  const app = fastify()
+  app.register(fastifyHtml)
+
+  app.get('/', async (_req, reply) => {
+    strictEqual(reply.html`${null}`, reply)
+    return reply
+  })
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, '')
+  }
+})
+
+test('renders a number', async t => {
+  const app = fastify()
+  app.register(fastifyHtml)
+
+  app.get('/', async (_req, reply) => {
+    strictEqual(reply.html`${42}`, reply)
+    return reply
+  })
+
+  {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, '42')
+  }
+})
+
+test('escape', async t => {
+  const app = fastify()
+  app.register(fastifyHtml)
+
+  app.post('/', async (req, reply) => {
+    const char = req.body.char
+    strictEqual(reply.html`<h1>Hello ${char}</h1>`, reply)
+    return reply
+  })
+
+  for (const char of ['<', '>', '"', '\'', '&']) {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/',
+      body: { char }
+    })
+    strictEqual(res.statusCode, 200)
+    strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+    strictEqual(res.body, `<h1>Hello ${escapeDictionary[char]}</h1>`)
+  }
 })
